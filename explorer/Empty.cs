4435d0b06ce97e422587e1273ed34c384735e6f3
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-12-14 11:07:35 +0100

    Create separate file for Empty class

---
+++ explorer/Empty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace explorer {
+    class Empty : Piece {
+        public Empty(int x, int y, char letter, Chessboard board) {
+            this.x = x;
+            this.y = y;
+            this.chessboard = board;
+            this.Letter = letter;
+        }
+
+        public override List<int[]> Moves(char[][] board) {
+            return null;
+        }
+    }
+}
--- explorer/Piece.cs
+++ explorer/Piece.cs
@@ -43,20 +43,4 @@ namespace explorer
         }
 
     }
-
-    class Empty : Piece
-    {
-        public Empty(int x, int y, char letter, Chessboard board)
-        {
-            this.x = x;
-            this.y = y;
-            this.chessboard = board;
-            this.Letter = letter;
-        }
-
-        public override List<int[]> Moves(char[][]board)
-        {
-            return null;
-        }
-    }
 }
